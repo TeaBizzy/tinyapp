ed43d5b7aaed2633ddb4b201c0a9a6917e4637cc
Stefan <satalbot@protonmail.com>
2022-11-03 18:22:24 +0000

    Fixed lint errors

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,13 +4,13 @@ const { getUserByEmail } = require('../helpers');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -18,7 +18,7 @@ const testUsers = {
 describe('getUserByEmail()', () => {
   it('should return the user object with the matching email', () => {
     const user = getUserByEmail('user@example.com', testUsers);
-    const expectedUserID = 'userRandomID'
+    const expectedUserID = 'userRandomID';
 
     expect(expectedUserID).to.be.equal(user['id']);
   });
