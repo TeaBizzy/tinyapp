ba92927ca5d46bb6065b729f57735f245c011f43
Stefan <satalbot@protonmail.com>
2022-11-03 18:16:04 +0000

    Implemented test for getUserByEmail(), expects to return correct user object

---
+++ test/helpersTest.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const { it } = require('mocha');
+const { getUserByEmail } = require('../helpers');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', () => {
+  it('should return the user object with the matching email', () => {
+    const user = getUserByEmail('user@example.com', testUsers);
+    const expectedUserID = 'userRandomID'
+
+    console.log(user);
+    expect(expectedUserID).to.be.equal(user['id']);
+  });
+});
