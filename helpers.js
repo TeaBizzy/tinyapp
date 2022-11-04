7d341922515fba15ca71549610ed680de84ff2be
Stefan <satalbot@protonmail.com>
2022-11-04 00:09:22 +0000

    Improved comments

--- helpers.js
+++ helpers.js
@@ -1,9 +1,16 @@
 // _________________________________________________________________________ //
 // *----------------------------- Information -----------------------------* //
-/*
-  Contains generic helper functions to be used anywhere in the project.
-*/
 
+// Contains generic helper functions to be used anywhere in the project.
+
+// _________________________________________________________________________ //
+// *------------------------------ Functions ------------------------------* //
+
+/*
+  Generates a random string consisting of upper and lower case letters
+  String length is determined by the given argument, default length of 6
+  Returns a string
+*/
 const generateRandomString = function(length = 6) {
   let randStr = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -17,6 +24,10 @@ const generateRandomString = function(length = 6) {
   return randStr;
 };
 
+/* 
+  Returns the user object from the database by matching email
+  Returns undefined if no user exists with the provided email
+*/ 
 const getUserByEmail = function(email, database) {
   for (const user in database) {
     if (database[user].email === email) {
@@ -26,6 +37,10 @@ const getUserByEmail = function(email, database) {
   return undefined;
 };
 
+/*
+  Returns all url objects from the database if the given userID matches
+  Returns empty object if no matches are found
+*/
 const getUrlsByUserID = function(userID, database) {
   const result = {};
   for (const url in database) {
